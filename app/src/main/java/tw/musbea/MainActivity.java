f951453318cb065c1240bcb8efb4eed4cb8152ea
thewolfprod <thewolf.kontakt@gmail.com>
2022-04-18 01:31:07 +0200

    First Upload on GitHub

---
+++ app/src/main/java/tw/musbea/MainActivity.java
@@ -0,0 +1,22 @@
+package tw.musbea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+ * APP MADED BY THEWOLF
+ * 18-04-2022 01:28
+ *
+ * THE IDEA OF THE APP IS WRITTEN AT:
+ * URL
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
